b5fb125009f0c5c9bdd784604f2d4a061fca3f34
Sam <k9052460210@gmail.com>
2019-03-18 20:58:30 +0800

    print index

--- GetPointer.py
+++ GetPointer.py
@@ -95,6 +95,7 @@ def GetWordByArticle(news):
 def GetWordBySheet():
 	sheetList = pd.DataFrame(columns = columnTitles)  # create empty list to store keywords for each sheet
 	for lineIndex in range(totalNewsNum):
+		print("lineIndex: ",lineIndex)
 		keywords = GetWordByArticle(df.loc[lineIndex])
 		for wordIndex in range(keywords.shape[0]):
 			keyword = keywords.loc[wordIndex]
