db34e8d2e0331281bc6c7415c298e3905dbf615e
Melhor <b05705046@ntu.edu.tw>
2019-03-18 14:55:27 +0800

    get pointer when devide news to group

---
+++ GetPointer.py
@@ -0,0 +1,166 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+import math
+
+patchLimit = 10
+xl = pd.ExcelFile("hw_text.xlsx") # <---------- change to `hw_text.xlsx`
+df = xl.parse("all") # <--------------------------- change to `all`
+totalNewsNum = df.shape[0]
+outputFile = "KeyWords.xlsx"
+fields = ["銀行", "信用卡", "匯率", "台積電", "台灣", "日本"]
+fieldNum = {"銀行":2, "信用卡":3, "匯率":5, 
+			"台積電":7, "台灣":11, "日本":13}  # match fields with num for labeling
+columnTitles = ["word" , "field",
+				"all_TF", "all_DF", "bank_TF", "bank_DF",
+				"card_TF", "card_DF", "rate_TF", "rate_DF",
+				"tsmc_TF", "tsmc_DF", "tw_TF", "tw_DF",
+				"japan_TF", "japan_DF"]
+pointers = ["word", "TF", "DF", "TF-IDF", "全部TF", "全部DF", "全部TF-IDF", "TF期望值", "DF期望值",
+			"TF卡方值", "DF卡方值", "MI(用DF)", "Lift(用DF)"]
+
+commenWords = [] 	# words that should not be keywords
+with open('commenWord.txt', encoding = "utf-8") as f:
+	commenWords = f.read().splitlines()
+
+def HasNumOrEn(string):
+	for ch in string:
+		# chinese is between \u4e00 and \u9fff
+		if(ch < '\u4e00' or ch > '\u9fff'):
+			return True
+	return False
+
+def CutContent(string):
+	wordList = pd.DataFrame(columns = ["word", "TF"])
+	for n in range(2,7):
+		for i in range(len(string)-n+1):
+			word = string[i:i+n]
+			if(not HasNumOrEn(word) and word not in commenWords):
+				if(word not in wordList["word"].tolist()):
+					newWord = {"word": word, "TF":1}
+					wordList = wordList.append(newWord, ignore_index = True)
+				else:
+					wordList.loc[wordList["word"] == word, "TF"] += 1
+
+	return wordList 	# a frame with each unique term's TF
+
+def CleanSheetList(keyList, lineIndex):
+	cleanList = keyList[keyList.all_DF > 1]
+	# if(lineIndex % 50 == 0):
+	# 	cleanList = cleanList[cleanList.all_TF >= 3]
+
+	return cleanList
+
+def GetFullColumnData(wordWithTF, belongField):
+	fullFrame = pd.DataFrame(columns = columnTitles)
+	data = {"word": wordWithTF["word"], "field": belongField, 
+			"all_TF": wordWithTF["TF"], "all_DF": 1}
+	for fieldIndex in range(len(fields)):
+		if(belongField % fieldNum[fields[fieldIndex]] == 0):
+			data[columnTitles[2*fieldIndex+4]] = wordWithTF["TF"]
+			data[columnTitles[2*fieldIndex+5]] = 1
+
+	fullFrame = fullFrame.append(data, ignore_index = True).fillna(0)
+	return fullFrame 		# a full frame of each term's index
+
+def MergeWordTF(titleWordTF, contentWordTF):
+	for lineIndex in range(titleWordTF.shape[0]):
+		wordinTitle = titleWordTF.loc[lineIndex, "word"]
+		if(wordinTitle not in contentWordTF["word"].tolist()):
+			contentWordTF = contentWordTF.append(titleWordTF.loc[lineIndex], ignore_index = True)
+		else:
+			contentWordTF.loc[contentWordTF["word"] == wordinTitle, "TF"] += titleWordTF.loc[lineIndex, "TF"]
+
+	return contentWordTF 	# a frame with unique term's TF from title, content
+
+def GetWordByArticle(news):
+	belongField = 1 	 # record the fields the news belongs to 
+	title = news["標題"]
+	content = news["內容"]
+	articleList = pd.DataFrame(columns = columnTitles)  # create empty list to store keywords in article
+	keywordPool = MergeWordTF(CutContent(title), CutContent(content))
+
+	for field in fields: 	# label its belonged fields
+		if (field in title or field in content):
+			belongField *= fieldNum[field]
+	
+	for lineIndex in range(keywordPool.shape[0]):
+		data = GetFullColumnData(keywordPool.loc[lineIndex], belongField)
+		articleList = articleList.append(data, ignore_index = True)
+
+	return articleList	# a full frame of pointers for unique words in news
+
+def GetWordBySheet():
+	sheetList = pd.DataFrame(columns = columnTitles)  # create empty list to store keywords for each sheet
+	for lineIndex in range(totalNewsNum):
+		keywords = GetWordByArticle(df.loc[lineIndex])
+		for wordIndex in range(keywords.shape[0]):
+			keyword = keywords.loc[wordIndex]
+			if(keyword["word"] not in sheetList["word"].tolist()):
+				sheetList = sheetList.append(keyword, ignore_index = True)
+			else:
+				for columnIndex in range(1, len(columnTitles)):
+					if(columnIndex == 1):
+						sheetList.loc[sheetList["word"] == keyword["word"], "field"] *= keyword["field"]
+					else:
+						sheetList.loc[sheetList["word"] == keyword["word"], columnTitles[columnIndex]] += keyword[columnTitles[columnIndex]]
+
+		if (lineIndex != 0 and (lineIndex % patchLimit == 0)):
+			sheetList = CleanSheetList(sheetList, lineIndex)
+	
+	return sheetList 	# a full dataFrame with all unique word's TF, DF in each field
+
+def GetTFIDF(tf, df, n):
+	log_tf = math.log10(tf)
+	log_Ndf = math.log10(float(n)/df)
+	return (1+log_tf)*log_Ndf
+
+def GetChiSquare(value, expectedValue):
+	chi = (value-expectedValue)**2
+	chi /= expectedValue
+	if(value < expectedValue):
+		chi *= -1
+
+	return chi
+
+def GetExpectedValue(value, fieldNewsNum):
+	return value * float(fieldNewsNum) / totalNewsNum
+
+def GetMI(wordDF, allDF, fieldNewsNum):
+	return math.log10(float(wordDF)/(allDF*fieldNewsNum))
+
+def GetLift(wordDF, allDF, fieldNewsNum):
+	upper = float(wordDF)/fieldNewsNum
+	lower = float(allDF)/totalNewsNum
+
+	return upper/lower
+
+def GetDataByField(rawData, fieldName):
+	fieldIndex = fields.index(fieldName)
+	data = rawData[rawData.field % fieldNum[fieldName] == 0] # column = columnTitles
+	fieldSheet = pd.DataFrame(columns = pointers)
+	fieldSheet["word"] = data["word"]
+	fieldSheet["TF"] = data[columnTitles[2*fieldIndex+4]]
+	fieldSheet["DF"] = data[columnTitles[2*fieldIndex+5]]
+	fieldSheet["全部TF"] = data["all_TF"]
+	fieldSheet["全部DF"] = data["all_DF"]
+
+	fieldNewsNum = data.shape[0]
+	for i in range(fieldNewsNum):
+		fieldSheet.loc[i, "TF-IDF"] = GetTFIDF(fieldSheet.loc[i, "TF"], fieldSheet.loc[i, "DF"], fieldNewsNum)
+		fieldSheet.loc[i, "全部TF-IDF"] = GetTFIDF(fieldSheet.loc[i, "全部TF"], fieldSheet.loc[i, "全部DF"], totalNewsNum)
+		fieldSheet.loc[i, "TF期望值"] = GetExpectedValue(fieldSheet.loc[i, "全部TF"], fieldNewsNum)
+		fieldSheet.loc[i, "DF期望值"] = GetExpectedValue(fieldSheet.loc[i, "全部DF"], fieldNewsNum)
+		fieldSheet.loc[i, "TF卡方值"] = GetChiSquare(fieldSheet.loc[i, "TF"], fieldSheet.loc[i, "全部TF"])
+		fieldSheet.loc[i, "DF卡方值"] = GetChiSquare(fieldSheet.loc[i, "DF"], fieldSheet.loc[i, "全部DF"])
+		fieldSheet.loc[i, "MI(用DF)"] = GetMI(fieldSheet.loc[i, "DF"], fieldSheet.loc[i, "全部DF"], fieldNewsNum)
+		fieldSheet.loc[i, "Lift(用DF)"] = GetLift(fieldSheet.loc[i, "DF"], fieldSheet.loc[i, "全部DF"], fieldNewsNum)
+
+	return fieldSheet
+		
+with pd.ExcelWriter(outputFile, mode='a+') as writer:
+	for field in fields:
+		data = GetDataByField(GetWordBySheet(),field)
+		print(field, "\n", data)
+		data.to_excel(writer, sheet_name= field, index = False, engine = 'xlsxwriter')
--- group1.py
+++ group1.py
@@ -14,150 +14,150 @@ inaccuracy = 0.01
 
 commenWords = [] # words that should not be keywords
 with open('commenWord.txt', encoding = "utf-8") as f:
-    commenWords = f.read().splitlines()
+	commenWords = f.read().splitlines()
 
 def HasNumOrEn(string):
-    for ch in string:
-        if(ch < '\u4e00' or ch > '\u9fff'):
-            return True
-    return False
+	for ch in string:
+		if(ch < '\u4e00' or ch > '\u9fff'):
+			return True
+	return False
 
 def DFisClose(num1, num2):
-    diff = float(abs(num1 - num2))
-    if(diff/max(num1, num2) <= inaccuracy):
-        return True
+	diff = float(abs(num1 - num2))
+	if(diff/max(num1, num2) <= inaccuracy):
+		return True
 
-    return False
+	return False
 
 def CutContent(string):
-    wordList = []
-    for n in range(2,7):
-        for i in range(len(string)-n+1):
-            word = string[i:i+n]
-            if(not HasNumOrEn(word) and word not in commenWords):
-                wordList.append(word)
+	wordList = []
+	for n in range(2,7):
+		for i in range(len(string)-n+1):
+			word = string[i:i+n]
+			if(not HasNumOrEn(word) and word not in commenWords):
+				wordList.append(word)
 
-    return wordList
+	return wordList
 
 def CleanSheetList(keyList):
-    # expectedTF = keyList["TF"].mean()
-    # cleanList = keyList[keyList.TF > expectedTF]
-    cleanList = keyList[keyList.TF >= 3]
+	# expectedTF = keyList["TF"].mean()
+	# cleanList = keyList[keyList.TF > expectedTF]
+	cleanList = keyList[keyList.TF >= 3]
 
-    return cleanList
+	return cleanList
 
 def GetWordByArticle(news):
-    articleList = pd.DataFrame(columns = columnTitles)  # create empty list to store keywords in article
-    title = news["標題"]
-    content = news["內容"]
-    keywordPool = CutContent(title)
-    keywordPool.extend(CutContent(content))
+	articleList = pd.DataFrame(columns = columnTitles)  # create empty list to store keywords in article
+	title = news["標題"]
+	content = news["內容"]
+	keywordPool = CutContent(title)
+	keywordPool.extend(CutContent(content))
 
-    for word in keywordPool:
-        if(word not in articleList["word"].tolist()):  # add word to list
-            news = {columnTitles[0]:word, columnTitles[1]:1, columnTitles[2]:1}
-            articleList.loc[articleList.shape[0]] = news
-        else:  # update `TF`
-            articleList.loc[articleList["word"] == word, "TF"] += 1
+	for word in keywordPool:
+		if(word not in articleList["word"].tolist()):  # add word to list
+			news = {columnTitles[0]:word, columnTitles[1]:1, columnTitles[2]:1}
+			articleList.loc[articleList.shape[0]] = news
+		else:  # update `TF`
+			articleList.loc[articleList["word"] == word, "TF"] += 1
 
-    return articleList
+	return articleList
 
 def GetWordBySheet(sheetName):
-    df = xl.parse(sheetName)
-    nrows = df.shape[0]
-    sheetList = pd.DataFrame(columns = columnTitles)  # create empty list to store keywords for each sheet
-    for lineIndex in range(nrows):
-        keywords = GetWordByArticle(df.loc[lineIndex])
-        for wordIndex in range(keywords.shape[0]):
-            keyword = keywords.loc[wordIndex]
-            if(keyword["word"] not in sheetList["word"].tolist()):
-                sheetList.loc[sheetList.shape[0]] = keyword
-            else:
-                sheetList.loc[sheetList["word"] == keyword["word"], "TF"] += keyword["TF"]
-                sheetList.loc[sheetList["word"] == keyword["word"], "DF"] += keyword["DF"]
+	df = xl.parse(sheetName)
+	nrows = df.shape[0]
+	sheetList = pd.DataFrame(columns = columnTitles)  # create empty list to store keywords for each sheet
+	for lineIndex in range(nrows):
+		keywords = GetWordByArticle(df.loc[lineIndex])
+		for wordIndex in range(keywords.shape[0]):
+			keyword = keywords.loc[wordIndex]
+			if(keyword["word"] not in sheetList["word"].tolist()):
+				sheetList.loc[sheetList.shape[0]] = keyword
+			else:
+				sheetList.loc[sheetList["word"] == keyword["word"], "TF"] += keyword["TF"]
+				sheetList.loc[sheetList["word"] == keyword["word"], "DF"] += keyword["DF"]
 
-        if (lineIndex != 0 and (lineIndex % patchLimit == 0)):
-            sheetList = CleanSheetList(sheetList)
-        print("len: ", sheetList.shape[0],"\n",sheetList.head())
-    return sheetList
+		if (lineIndex != 0 and (lineIndex % patchLimit == 0)):
+			sheetList = CleanSheetList(sheetList)
+		print("len: ", sheetList.shape[0],"\n",sheetList.head())
+	return sheetList
 
 def AddPointer(keywordList):
-    n = keywordList.shape[0]
-    for i in range(n):
-        log_tf = math.log10(keywordList.loc[i,"TF"])
-        log_Ndf = math.log10(float(n)/keywordList.loc[i,"DF"])
-        keywordList.loc[i,"TF-IDF"] = (1+log_tf)*log_Ndf
+	n = keywordList.shape[0]
+	for i in range(n):
+		log_tf = math.log10(keywordList.loc[i,"TF"])
+		log_Ndf = math.log10(float(n)/keywordList.loc[i,"DF"])
+		keywordList.loc[i,"TF-IDF"] = (1+log_tf)*log_Ndf
 
-    return keywordList
+	return keywordList
 
 def lcs(str_a, str_b):
-    if len(str_a) == 0 or len(str_b) == 0:
-        return 0
+	if len(str_a) == 0 or len(str_b) == 0:
+		return 0
   
-    lcs_str=""
-    max_len = 0
+	lcs_str=""
+	max_len = 0
   
-    dp = [0 for _ in range(len(str_b) + 1)]
-    for i in range(1, len(str_a) + 1):
-        left_up = 0
-        for j in range(1, len(str_b) + 1):
-            up = dp[j]
-            if str_a[i-1] == str_b[j-1]:
-                dp[j] = left_up + 1
-                max_len = max([max_len, dp[j]])
-                if max_len == dp[j]:
-                    lcs_str = str_a[i-max_len:i]
-            else:
-                dp[j] = 0
-            left_up = up
-    return(lcs_str)
+	dp = [0 for _ in range(len(str_b) + 1)]
+	for i in range(1, len(str_a) + 1):
+		left_up = 0
+		for j in range(1, len(str_b) + 1):
+			up = dp[j]
+			if str_a[i-1] == str_b[j-1]:
+				dp[j] = left_up + 1
+				max_len = max([max_len, dp[j]])
+				if max_len == dp[j]:
+					lcs_str = str_a[i-max_len:i]
+			else:
+				dp[j] = 0
+			left_up = up
+	return(lcs_str)
 
 # find longest common substring
 def merge(str_a, str_b):   
-    result=""
-    dup=lcs(str_a,str_b)
-    s1=str_a.find(dup)
-    s2=str_b.find(dup)
-    e1=s1+len(dup)
-    e2=s2+len(dup)
+	result=""
+	dup=lcs(str_a,str_b)
+	s1=str_a.find(dup)
+	s2=str_b.find(dup)
+	e1=s1+len(dup)
+	e2=s2+len(dup)
   
-    # different order condition 
-    if(s1 > s2 & e1 and len(str_a)):
-        result=str_a+str_b[s2+len(dup):len(str_b)]
-    if(s2 > s1 & e2 and len(str_b)):
-        result=str_b+str_a[s1+len(dup):len(str_a)]
+	# different order condition 
+	if(s1 > s2 & e1 and len(str_a)):
+		result=str_a+str_b[s2+len(dup):len(str_b)]
+	if(s2 > s1 & e2 and len(str_b)):
+		result=str_b+str_a[s1+len(dup):len(str_a)]
   
-    return result
+	return result
 
 def RemoveDuplicateKeyword(keywordList):
-    tempList = keywordList
-    for i in range(keywordList.shape[0]):
-        for j in range(i+1, keywordList.shape[0]):
-            key1 = keywordList.loc[i, "word"]
-            key2 = keywordList.loc[j, "word"]
-            if(merge(key1, key2) != ""):
-                DF1 = keywordList.loc[keywordList["word"] == key1,"DF"].values[0]
-                DF2 = keywordList.loc[keywordList["word"] == key2,"DF"].values[0]
-                if(DFisClose(DF1,DF2)): # merge two rows
-                    mergedWord = merge(key1,key2)
-                    mergedNews = {"word": mergedWord, 
-                                  "TF": keywordList.loc[keywordList["word"] == key1,"TF"].values[0],
-                                  "DF": DF1}
-                    if(DF1 < DF2):
-                        mergedNews["TF"] = keywordList.loc[keywordList["word"] == key2,"TF"].values[0]
-                        mergedNews["DF"] = DF2
-                    
-                    tempList = tempList[tempList.word != key1].reset_index(drop=True)
-                    tempList = tempList[tempList.word != key2].reset_index(drop=True)
-                    tempList = tempList.append(mergedNews, ignore_index = True)
-    tempList = tempList[tempList.TF > 3]
+	tempList = keywordList
+	for i in range(keywordList.shape[0]):
+		for j in range(i+1, keywordList.shape[0]):
+			key1 = keywordList.loc[i, "word"]
+			key2 = keywordList.loc[j, "word"]
+			if(merge(key1, key2) != ""):
+				DF1 = keywordList.loc[keywordList["word"] == key1,"DF"].values[0]
+				DF2 = keywordList.loc[keywordList["word"] == key2,"DF"].values[0]
+				if(DFisClose(DF1,DF2)): # merge two rows
+					mergedWord = merge(key1,key2)
+					mergedNews = {"word": mergedWord, 
+								  "TF": keywordList.loc[keywordList["word"] == key1,"TF"].values[0],
+								  "DF": DF1}
+					if(DF1 < DF2):
+						mergedNews["TF"] = keywordList.loc[keywordList["word"] == key2,"TF"].values[0]
+						mergedNews["DF"] = DF2
+					
+					tempList = tempList[tempList.word != key1].reset_index(drop=True)
+					tempList = tempList[tempList.word != key2].reset_index(drop=True)
+					tempList = tempList.append(mergedNews, ignore_index = True)
+	tempList = tempList[tempList.TF > 3]
 
-    return tempList.sort_values("TF", ascending=False).reset_index(drop=True)
+	return tempList.sort_values("TF", ascending=False).reset_index(drop=True)
 
 
 with pd.ExcelWriter(outputFile, mode='a+') as writer:
-    # keywordSheet = AddPointer(RemoveDuplicateKeyword(GetWordBySheet(sheets[0])))
-    # keywordSheet.to_excel(writer, sheet_name= sheets[0], index = False, engine = 'xlsxwriter')
-    for sheet in sheets:
-        keywordSheet = AddPointer(RemoveDuplicateKeyword(GetWordBySheet(sheet)))
-        keywordSheet.to_excel(writer, sheet_name= sheet, index = False, engine = 'xlsxwriter')
+	# keywordSheet = AddPointer(RemoveDuplicateKeyword(GetWordBySheet(sheets[0])))
+	# keywordSheet.to_excel(writer, sheet_name= sheets[0], index = False, engine = 'xlsxwriter')
+	for sheet in sheets:
+		keywordSheet = AddPointer(RemoveDuplicateKeyword(GetWordBySheet(sheet)))
+		keywordSheet.to_excel(writer, sheet_name= sheet, index = False, engine = 'xlsxwriter')
